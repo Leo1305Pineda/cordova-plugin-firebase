a182558d960b6a7247a261b76dd5b331da0676f6
Leonardo Pineda <leo1305pineda@gmail.com>
2019-05-07 13:58:04 -0400

    FirebaseMessagingService

--- src/android/FirebasePluginInstanceIDService.java
+++ src/android/FirebasePluginInstanceIDService.java
@@ -2,10 +2,9 @@ package org.apache.cordova.firebase;
 
 import android.util.Log;
 
-import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.google.firebase.messaging.FirebaseMessagingService;
 
-public class FirebasePluginInstanceIDService extends FirebaseInstanceIdService {
+public class FirebasePluginInstanceIDService extends FirebaseMessagingService {
 
     private static final String TAG = "FirebasePlugin";
 
@@ -15,11 +14,9 @@ public class FirebasePluginInstanceIDService extends FirebaseInstanceIdService {
      * is initially generated so this is where you would retrieve the token.
      */
     @Override
-    public void onTokenRefresh() {
+    public void onNewToken(String refreshedToken) {
         // Get updated InstanceID token.
-        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
         Log.d(TAG, "Refreshed token: " + refreshedToken);
-
         FirebasePlugin.sendToken(refreshedToken);
     }
 }
